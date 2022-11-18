3e0a675fc9643cacc205eb70d5b768f1acad6c12
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-11-18 10:54:35 -0800

    Add job run

--- src/main/java/gov/cms/ab2d/contracts/quartz/HPMSIngestJob.java
+++ src/main/java/gov/cms/ab2d/contracts/quartz/HPMSIngestJob.java
@@ -1,6 +1,12 @@
 package gov.cms.ab2d.contracts.quartz;
 
+import com.amazonaws.services.sns.model.ResourceNotFoundException;
+import gov.cms.ab2d.contracts.model.Property;
+import gov.cms.ab2d.contracts.repository.PropertiesRepository;
 import gov.cms.ab2d.contracts.service.AttestationUpdaterService;
+import gov.cms.ab2d.contracts.service.FeatureEngagement;
+import java.util.Optional;
+import java.util.Properties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
@@ -12,21 +18,21 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 @RequiredArgsConstructor
 @DisallowConcurrentExecution
 public class HPMSIngestJob extends QuartzJobBean {
-
+    public static final String HPMS_INGESTION_ENGAGEMENT = "hpms.ingest.engaged";
     private final AttestationUpdaterService aus;
-//    private final PropertiesAPIService propertiesApiService;
+    private final PropertiesRepository propertiesRepository;
 
     @SuppressWarnings("NullableProblems")
     @Override
     protected void executeInternal(JobExecutionContext context) {
         // todo additionally filter hpms engagement by tracking the environment ab2d is running in.
         //      if the environment is not sandbox or prod do not poll hpms
-//        if (FeatureEngagement.IN_GEAR == getEngagement()) {
-//            aus.pollOrganizations();
-//        }
+        if (FeatureEngagement.IN_GEAR == getEngagement()) {
+            aus.pollOrganizations();
+        }
     }
 
-//    public FeatureEngagement getEngagement() {
-//        return FeatureEngagement.fromString(propertiesApiService.getProperty(HPMS_INGESTION_ENGAGEMENT));
-//    }
+    public FeatureEngagement getEngagement() {
+        return FeatureEngagement.fromString(propertiesRepository.findByKey(HPMS_INGESTION_ENGAGEMENT).toString());
+    }
 }
