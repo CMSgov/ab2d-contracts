024880463996946a9c45421d7c88008d545b39e3
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2023-01-20 15:34:29 -0800

    Remove properties repository

--- src/main/java/gov/cms/ab2d/contracts/quartz/HPMSIngestJob.java
+++ src/main/java/gov/cms/ab2d/contracts/quartz/HPMSIngestJob.java
@@ -1,6 +1,5 @@
 package gov.cms.ab2d.contracts.quartz;
 
-import gov.cms.ab2d.contracts.repository.PropertiesRepository;
 import gov.cms.ab2d.contracts.service.AttestationUpdaterService;
 import gov.cms.ab2d.contracts.service.FeatureEngagement;
 import gov.cms.ab2d.properties.client.PropertiesClient;
@@ -19,13 +18,9 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 public class HPMSIngestJob extends QuartzJobBean {
     public static final String HPMS_INGESTION_ENGAGEMENT = "hpms.ingest.engaged";
     private final AttestationUpdaterService aus;
-    private final PropertiesRepository propertiesRepository;
 
     @Value("${property.service.url}")
     private String propertyServiceUrl;
-
-    @Value("${feature.property.service.enabled}")
-    private boolean propertiesFlag;
 
     private final PropertiesClient propertiesClient = new PropertiesClientImpl(propertyServiceUrl);
 
@@ -41,12 +36,6 @@ public class HPMSIngestJob extends QuartzJobBean {
     }
 
     public FeatureEngagement getEngagement() {
-        if (propertiesFlag) {
-            //TODO Enable when properties service is available
             return FeatureEngagement.fromString(propertiesClient.getProperty(HPMS_INGESTION_ENGAGEMENT).toString());
-        } else {
-            //TODO Delete when properties service is available
-            return FeatureEngagement.fromString(propertiesRepository.findByKey(HPMS_INGESTION_ENGAGEMENT).toString());
-        }
     }
 }
