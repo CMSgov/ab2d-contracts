1d19ea798efd085bb5821234fad0d1e8ae5d1b7c
Sadibhatla <121067659+Sadibhatla@users.noreply.github.com>
2025-01-29 14:30:20 -0800

    Update github-actions-release.sh

--- scripts/github-actions-release.sh
+++ scripts/github-actions-release.sh
@@ -7,4 +7,4 @@ set -e # Turn on exit on error
 #
 ######## update ECS service with new image ################
 #
-aws ecs update-service --cluster $DEPLOYMENT_ENV-microservice-cluster  --service ab2d-event-service --force-new-deployment
+aws ecs update-service --cluster $DEPLOYMENT_ENV-microservice-cluster  --service ab2d-contract-service --force-new-deployment
